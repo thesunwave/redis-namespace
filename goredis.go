e3acd732bf2bd498dee448dcb19f8f7b3f2bed6b
Artur Borisovich <thesunwave@users.noreply.github.com>
2020-07-06 14:57:26 +0300

    [WIP]: Implement redis.Cmdble methods with namespaces (#2)
    * Start to implement
    
    * Add Exists, Expire, ExpireAt
    
    * Add ObjectRefCount, LPush
    
    * Add ObjectEncoding, ObjectIdleTime, Persist, PExpire, PExpireAt, PTTL.
    Remove several administrative commands
    
    * Update go.yml
    
    * Add Rename, RenameNX.
    
    * Cosmetic maintenance

--- goredis.go
+++ goredis.go
@@ -9,30 +9,56 @@ import (
 )
 
 type Client struct {
-	GoRedis
-	client    GoRedis
+	redis.Cmdable
 	namespace string
 }
 
-type GoRedis interface {
-	redis.Cmdable
+func NewGoRedisWithNamespace(namespace string, client redis.Cmdable) *Client {
+	return &Client{client, namespace + ":"}
 }
 
-func NewGoRedisWithNamespace(namespace string, client GoRedis) *Client {
-	return &Client{client, client, namespace + ":"}
+func (c *Client) Del(keys ...string) *redis.IntCmd {
+	var wrappedKeys []string
+
+	for _, key := range keys {
+		wrappedKeys = append(wrappedKeys, c.namespace+key)
+	}
+	return c.Cmdable.Del(wrappedKeys...)
 }
 
-func (c *Client) Get(key string) *redis.StringCmd {
-	return c.client.Get(c.namespace + key)
+func (c *Client) Unlink(keys ...string) *redis.IntCmd {
+	var wrappedKeys []string
+
+	for _, key := range keys {
+		wrappedKeys = append(wrappedKeys, c.namespace+key)
+	}
+	return c.Cmdable.Unlink(wrappedKeys...)
 }
 
-func (c *Client) Set(key string, value interface{}, expiration time.Duration) *redis.StatusCmd {
-	return c.client.Set(c.namespace+key, value, expiration)
+func (c *Client) Dump(key string) *redis.StringCmd {
+	return c.Cmdable.Dump(c.namespace + key)
+}
+
+func (c *Client) Exists(keys ...string) *redis.IntCmd {
+	var wrappedKeys []string
+
+	for _, key := range keys {
+		wrappedKeys = append(wrappedKeys, c.namespace+key)
+	}
+	return c.Cmdable.Exists(wrappedKeys...)
+}
+
+func (c *Client) Expire(key string, expiration time.Duration) *redis.BoolCmd {
+	return c.Cmdable.Expire(c.namespace+key, expiration)
+}
+
+func (c *Client) ExpireAt(key string, tm time.Time) *redis.BoolCmd {
+	return c.Cmdable.ExpireAt(c.namespace+key, tm)
 }
 
 func (c *Client) Keys(pattern string) *redis.StringSliceCmd {
 	var unwrappedKeys []string
-	keys := c.client.Keys(c.namespace + pattern)
+	keys := c.Cmdable.Keys(c.namespace + pattern)
 
 	for _, key := range keys.Val() {
 		unwrappedKeys = append(unwrappedKeys, strings.TrimPrefix(key, c.namespace))
@@ -46,4 +72,52 @@ func (c *Client) Keys(pattern string) *redis.StringSliceCmd {
 	*realPtrToY = unwrappedKeys
 
 	return keys
+}
+
+func (c *Client) ObjectRefCount(key string) *redis.IntCmd {
+	return c.Cmdable.ObjectRefCount(c.namespace + key)
+}
+
+func (c *Client) ObjectEncoding(key string) *redis.StringCmd {
+	return c.Cmdable.ObjectEncoding(c.namespace + key)
+}
+
+func (c *Client) ObjectIdleTime(key string) *redis.DurationCmd {
+	return c.Cmdable.ObjectIdleTime(c.namespace + key)
+}
+
+func (c *Client) Persist(key string) *redis.BoolCmd {
+	return c.Cmdable.Persist(c.namespace + key)
+}
+
+func (c *Client) PExpire(key string, expiration time.Duration) *redis.BoolCmd {
+	return c.Cmdable.PExpire(c.namespace+key, expiration)
+}
+
+func (c *Client) PExpireAt(key string, tm time.Time) *redis.BoolCmd {
+	return c.Cmdable.PExpireAt(c.namespace+key, tm)
+}
+
+func (c *Client) PTTL(key string) *redis.DurationCmd {
+	return c.Cmdable.PTTL(c.namespace + key)
+}
+
+func (c *Client) Rename(key, newkey string) *redis.StatusCmd {
+	return c.Cmdable.Rename(c.namespace+key, c.namespace+newkey)
+}
+
+func (c *Client) RenameNX(key, newkey string) *redis.BoolCmd {
+	return c.Cmdable.RenameNX(c.namespace+key, c.namespace+newkey)
+}
+
+func (c *Client) Get(key string) *redis.StringCmd {
+	return c.Cmdable.Get(c.namespace + key)
+}
+
+func (c *Client) Set(key string, value interface{}, expiration time.Duration) *redis.StatusCmd {
+	return c.Cmdable.Set(c.namespace+key, value, expiration)
+}
+
+func (c *Client) LPush(key string, values ...interface{}) *redis.IntCmd {
+	return c.Cmdable.LPush(c.namespace+key, values...)
 }
--- goredis_test.go
+++ goredis_test.go
@@ -1,10 +1,12 @@
 package redis_namespace
 
 import (
-	"github.com/alicebob/miniredis"
+	"fmt"
+	_ "github.com/alicebob/miniredis"
 	"github.com/go-redis/redis/v7"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 // Define the suite, and absorb the built-in basic suite
@@ -13,19 +15,21 @@ import (
 type ClientTestSuite struct {
 	suite.Suite
 
-	redisClient              GoRedis
+	redisClient              redis.Cmdable
 	redisClientWithNamespace *Client
+
+	namespace string
 }
 
 func (s *ClientTestSuite) SetupTest() {
-	mr, err := miniredis.Run()
-	s.Require().NoError(err)
-
 	s.redisClient = redis.NewClient(&redis.Options{
-		Addr: mr.Addr(),
+		Addr: "localhost:6379",
 	})
 
-	s.redisClientWithNamespace = NewGoRedisWithNamespace("test_namespace", s.redisClient)
+	s.namespace = "test_namespace"
+
+	s.redisClientWithNamespace = NewGoRedisWithNamespace(s.namespace, s.redisClient)
+	s.redisClient.FlushAll()
 }
 
 func (s *ClientTestSuite) TestPing() {
@@ -43,6 +47,148 @@ func (s *ClientTestSuite) TestGet_Set() {
 
 	resultGet := s.redisClientWithNamespace.Get("key")
 	s.Equal(resultGet.Val(), "value")
+}
+
+func (s *ClientTestSuite) TestClient_Del() {
+	resultSet := s.redisClientWithNamespace.Set("key", "value", 0)
+	s.Nil(resultSet.Err())
+	resultSet = s.redisClientWithNamespace.Set("key2", "value", 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Del("key", "key2")
+	s.EqualValues(2, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_Unlink() {
+	resultSet := s.redisClientWithNamespace.Set("key", "value", 0)
+	s.Nil(resultSet.Err())
+	resultSet = s.redisClientWithNamespace.Set("key2", "value", 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Unlink("key", "key2")
+	s.Nil(result.Err())
+	s.EqualValues(2, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_Dump() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Dump("key")
+	s.Nil(result.Err())
+	s.EqualValues("\u0000\xC0\n\t\u0000\xBEm\u0006\x89Z(\u0000\n", result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_Exists() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Exists("key", "nosuchkey")
+	s.Nil(result.Err())
+	s.EqualValues(1, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_Expire() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Expire("key", 10*time.Second)
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_ExpireAt() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.ExpireAt("key", time.Now().Add(10*time.Second))
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_ObjectRefCount() {
+	resultSet := s.redisClientWithNamespace.LPush("mylist", "value")
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.ObjectRefCount("mylist")
+	s.Nil(result.Err())
+	s.EqualValues(1, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_ObjectEncoding() {
+	resultSet := s.redisClientWithNamespace.Set("key", "value", 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.ObjectEncoding("key")
+	s.Nil(result.Err())
+	s.EqualValues("embstr", result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_ObjectIdleTime() {
+	resultSet := s.redisClientWithNamespace.Set("key", "value", 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.ObjectIdleTime("key")
+	s.Nil(result.Err())
+	s.EqualValues(0, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_Persist() {
+	resultSet := s.redisClientWithNamespace.Set("key", "value", 10*time.Second)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Persist("key")
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_PExpire() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.PExpire("key", 10*time.Second)
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_PExpireAt() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.PExpireAt("key", time.Now().Add(10*time.Second))
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_PTTL() {
+	// Need to mock that case
+	//resultSet := s.redisClientWithNamespace.Set("key", 10, 1 * time.Second)
+	//s.Nil(resultSet.Err())
+	//
+	//result := s.redisClientWithNamespace.PTTL("key")
+	//s.Nil(result.Err())
+	//s.EqualValues(999, result.Val()) // => we can't do that because of using real Redis connection
+}
+
+func (s *ClientTestSuite) TestClient_Rename() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.Rename("key", "newkey")
+	s.Nil(result.Err())
+	s.EqualValues("OK", result.Val())
+}
+
+func (s *ClientTestSuite) TestClient_RenameNX() {
+	resultSet := s.redisClientWithNamespace.Set("key", 10, 0)
+	s.Nil(resultSet.Err())
+
+	result := s.redisClientWithNamespace.RenameNX("key", "newkey")
+	s.Nil(result.Err())
+	s.EqualValues(true, result.Val())
+
+	key := s.redisClient.Get(fmt.Sprintf("%s:%s", s.namespace, "newkey"))
+	s.Nil(key.Err())
 }
 
 func TestClientTestSuite(t *testing.T) {
